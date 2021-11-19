8cfa2cc82232a4b7d11f4fb7d41884454c44e110
Namydad <radioactive.esports.gaming@gmail.com>
2021-11-19 20:23:53 +0200

    updated

--- Chess-Rice-Problem.py
+++ Chess-Rice-Problem.py
@@ -7,6 +7,4 @@ y = 1
 for chessboardspaces in range(1,65,1):
     x=y+(y-1)
     print("Anzahl auf Feld Nummer " + str(chessboardspaces)+" ist gleich = "+str(y)+" Gesamt: "+str(x))
-    y=y*2
-    if chessboardspaces == 64:
-        print("Anzahl auf Feld Nummer " + str(chessboardspaces)+" ist gleich = "+str(y)+" Gesamt: "+str(x))
+    y=y*
