ff83a1c88075d6aad0a7c61ef66eea32a694521d
Namydad <radioactive.esports.gaming@gmail.com>
2021-12-03 20:55:57 +0100

    updated fixed extra task

--- KI_Zahlenraten.py
+++ KI_Zahlenraten.py
@@ -1,13 +1,15 @@
 import random
 nr = int(input("Welche Zahl soll von 0 bis 99 soll erraten werden? "))
+guessing = True
 guess = 0
 y = 0 
 x = 99
-for guess in range(0,10,1):
+while guessing==True:
     algorithmguess = random.randint(y,x)
+    guess = guess+1
     if algorithmguess == nr:
         print("Ihre Zahl " + str(nr) + " wurde erraten")
-        guess = 11
+        guess=11
     else:
         whatisit = int(input("Ist die Zahl größer(1) oder kleiner(2) als " + str(algorithmguess) + "?"))
         if whatisit==1:
@@ -16,12 +18,11 @@ for guess in range(0,10,1):
             x=algorithmguess-1
         else:
             print("Wrong Input new Guess made!")
-            guess = -1
-"""
     if guess == 11:
         again = int(input("Wollen Sie nochmal spielen? (0/1)"))
         if again == 0:
             print("Thanks for Playing!")
+            guessing = False
         if again == 1:
             print("Time for another round!")
             guess = 0
@@ -29,6 +30,5 @@ for guess in range(0,10,1):
             x = 99
             algorithmguess = 0
             nr = int(input("Welche Zahl soll von 0 bis 99 soll erraten werden? "))
-"""
 
     
