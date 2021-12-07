efb308ccc383192d6cae1d085ca453f7b83e6412
Namydad <radioactive.esports.gaming@gmail.com>
2021-12-07 08:58:59 +0100

    Orcun's Version

---
+++ Zahlenraten (Orcun).py
@@ -0,0 +1,20 @@
+import random
+x = 0
+y = 99
+while True:
+    r=random.randint(x,y)
+    print(r)
+    frage=int(input("[9] für größer als meine Zahl [1] für kleiner als meine Zahl [5] für Identisch"))
+
+    if frage==9:
+        y=r-1
+    elif frage==1:
+        x=r+1
+    elif frage==5:
+        print("Das Programm hat gewonnen")
+        again=str(input("Wollen sie nochmal spielen [J/N]"))
+        if again == 'J':
+            x = 0
+            y = 99
+        else:
+            print("Danke fürs Spielen")
