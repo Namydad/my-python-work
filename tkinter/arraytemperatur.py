520fe69bfd8f27fa6a5ba0b26203eb9178eaac04
Namydad <radioactive.esports.gaming@gmail.com>
2022-03-08 09:45:48 +0100

    w

--- tkinter/arraytemperatur.py
+++ tkinter/arraytemperatur.py
@@ -1,6 +1,7 @@
 import tkinter
 from tkinter import *
 import math
+from unittest import TestProgram
 
 """window = Tk()
 window.geometry("600x800")
@@ -11,17 +12,37 @@ temperatur=[0,2,3,5,6,1,2,3,4]
 def arrayAusgabe():
     print(temperatur)
     print("Länge " + str(len(temperatur)))
-    
+
     for i in range(0,len(temperatur)):
         print(temperatur[i])
 
+
 """    for element in temperatur:
         print(element)      """
+
+"""
+max < temperatur[i]
+max <- temperatur
+
+min > temperatur[i]
+min <- temperatur
+
+summe=summe+temperatur[i]
+
+mittelwert=summe/len(temperatur)
+
+"""
+
 print(arrayAusgabe())
+
 print("The Minimum is ",end="")
 print(min(temperatur))
+
 print("The Maximum is ",end="")
 print(max(temperatur))
 
+avg=sum(temperatur)/len(temperatur)
+print("The Average is ",end="")
+print(avg)
 
 """window.mainloop()"""
