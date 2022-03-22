fdf8b37d8b31412ea45a8d6ec0daeb19aa99274f
Namydad <radioactive.esports.gaming@gmail.com>
2022-03-22 08:49:58 +0100

    s

--- tkinter/paint.py
+++ tkinter/paint.py
@@ -1,17 +1,23 @@
 import tkinter
 from tkinter import *
 
+
 window = Tk()
 window.geometry("600x800")
 window.title("Parabel")
 
 def painting(event):
-    x=event.x()
-    y=event.y()
+    x=event.x
+    y=event.y
 
-fenster.bind("<Button-1>", painting)
-fenster.bind("<ButtonRelease-1>", painting)
-fenster.bind("<B1-Motion>", painting)
+window.bind("<Button-1>", painting)
+window.bind("<ButtonRelease-1>", painting)
+window.bind("<B1-Motion>", painting)
 
+canvasGrafik = tkinter.Canvas(window, width=500, height=400, bg='white')
+canvasGrafik.grid()
+canvasGrafik.place(x=50,y=250)
+canvasGrafik.create_line(10,0,10,400)
+canvasGrafik.create_line(10,200,500,200)
 
 window.mainloop()
