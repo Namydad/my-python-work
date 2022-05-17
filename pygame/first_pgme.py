875fa6b2e7b81fe6b6636ad5dfc2fdb9ea534c63
Namydad <radioactive.esports.gaming@gmail.com>
2022-05-17 09:00:05 +0200

    pygame test

--- pygame/first_pgme.py
+++ pygame/first_pgme.py
@@ -16,7 +16,8 @@ clock=pygame.time.Clock()
 ballpos_x = 300
 ballpos_y = 30
 BALL_DURCHMESSER = 20
-bewegung_y = 1
+bewegung_y = 3
+bewegung_x = 3
 
 while spielaktiv:
     for event in pygame.event.get():
@@ -26,18 +27,19 @@ while spielaktiv:
     screen.fill(GRUEN)
     pygame.draw.ellipse(screen,ROT,[ballpos_x,ballpos_y,BALL_DURCHMESSER,BALL_DURCHMESSER])
     ballpos_y += bewegung_y
-    if ballpos_y >= FENSTERHOEHE - BALL_DURCHMESSER or ballpos_y:
+    if ballpos_y >= FENSTERHOEHE - BALL_DURCHMESSER or ballpos_y<=0:
         bewegung_y = bewegung_y *(-1)
-
-    """""
-    if ballpos_y < FENSTERHOEHE - BALL_DURCHMESSER:
-        ballpos_y += bewegung_y
-    if ballpos_y == 380:
-        ballpos_y -= bewegung_y
-    """
-
+    ballpos_x += bewegung_x
+    if ballpos_x >= FENSTERBREITE - BALL_DURCHMESSER or ballpos_x<=0:
+        bewegung_x = bewegung_x *(-1)
     pygame.display.flip()
     clock.tick(60)
 
 pygame.quit()
 quit()
+"""    
+    if ballpos_y < FENSTERHOEHE - BALL_DURCHMESSER:
+        ballpos_y += bewegung_y
+    if ballpos_y == 380:
+        ballpos_y -= bewegung_y
+"""
